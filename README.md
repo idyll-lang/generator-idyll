a5c118943ad91168770a35f49abbfa0935d7ee97
Matthew Conlen <mc@mathisonian.com>
2017-02-11 15:41:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# generator-idyll
+
+Yeoman generator for Idyll projects
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-idyll using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-idyll
+```
+
+Then generate your new project:
+
+```bash
+yo idyll
+```
+
+Feel free to learn more about Yeoman at http://yeoman.io/
+
+## License
+
+MIT © [Matthew Conlen](mathisonian.com)
---
+++ generators/app/index.js
@@ -0,0 +1,50 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+
+module.exports = Generator.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('generator-idyll') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'text',
+      name: 'name',
+      message: 'Project name (I\'ll create a new folder with this name and put the generated files inside of it): ',
+      default: 'idyll-project'
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copyTpl(this.templatePath('_package.json'),  this.destinationPath(this.props.name + '/' + 'package.json'), this.props);
+
+    this.fs.copy(
+      this.templatePath('index.idl'),
+      this.destinationPath(this.props.name + '/' + 'index.idl')
+    );
+    this.fs.copy(
+      this.templatePath('styles.css'),
+      this.destinationPath(this.props.name + '/' + 'styles.css')
+    );
+  },
+
+  install: function () {
+    var elementDir = path.join(process.cwd(), this.props.name);
+    process.chdir(elementDir);
+    var self = this;
+    this.npmInstall(undefined, undefined, function() {
+      self.log('Your new project was created. To get started run `cd ' + self.props.name + ' && npm start`');
+    });
+
+
+  }
+});
