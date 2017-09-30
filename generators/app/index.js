e069bd7fab6d31b5bf37787d5ae886a1706f3da7
Matthew Conlen <mc@mathisonian.com>
2017-09-30 11:36:17 -0700

    updates for v2

--- generators/app/index.js
+++ generators/app/index.js
@@ -53,10 +53,6 @@ module.exports = Generator.extend({
       this.props
     );
     this.fs.copy(
-      this.templatePath('_index.html'),
-      this.destinationPath(this.props.name + '/_index.html')
-    );
-    this.fs.copy(
       this.templatePath('styles.css'),
       this.destinationPath(this.props.name + '/styles.css')
     );
@@ -68,7 +64,6 @@ module.exports = Generator.extend({
       this.templatePath('data/'),
       this.destinationPath(this.props.name + '/data')
     );
-    mkdirp.sync(this.destinationPath(this.props.name + '/components/default'));
     mkdirp.sync(this.destinationPath(this.props.name + '/build'));
     mkdirp.sync(this.destinationPath(this.props.name + '/images'));
     mkdirp.sync(this.destinationPath(this.props.name + '/fonts'));
@@ -85,15 +80,7 @@ module.exports = Generator.extend({
         self.log(err);
         return;
       }
-      try {
-        self.fs.copy(
-          self.destinationPath(self.props.name + '/node_modules/idyll-default-components/components/'),
-          self.destinationPath(self.props.name + '/components/default')
-        );
-        self.log('Your new project was created. To get started run `cd ' + self.props.name + ' && npm start`');
-      } catch (e) {
-        self.log(e);
-      }
+      self.log('Your new project was created. To get started run `cd ' + self.props.name + ' && npm start`');
     }});
   }
 });
